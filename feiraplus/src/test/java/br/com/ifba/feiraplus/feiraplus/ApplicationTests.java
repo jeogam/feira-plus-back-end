51ceea934931215fccd2c8ee2ea9e22f22b6302c
Jeovani Stars <jeovanigamati@gmail.com>
2025-11-12 13:03:29 -0300

    feat(project): Aplicando projeto SpringBoot ao Back-End

---
+++ feiraplus/src/test/java/br/com/ifba/feiraplus/feiraplus/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifba.feiraplus.feiraplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
