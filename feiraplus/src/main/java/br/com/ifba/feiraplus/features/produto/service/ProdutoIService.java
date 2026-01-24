7af8546bc293ef3e91ba6a2caee96ed259369773
gervizdev <guilherme@gmail.com>
2026-01-24 09:24:03 -0300

    feat(produto): implementa módulo Produto completo (SCRUM-17)
    - Cria entidade Produto com nome, preço, foto e descrição
    - Implementa ProdutoRepository, Service e Controller
    - Adiciona DTOs de request e response com validações
    - Configura endpoints REST com segurança para ADMIN e EXPOSITOR
    - Atualiza .gitignore para debug files

---
+++ feiraplus/src/main/java/br/com/ifba/feiraplus/features/produto/service/ProdutoIService.java
@@ -0,0 +1,14 @@
+package br.com.ifba.feiraplus.features.produto.service;
+
+import br.com.ifba.feiraplus.features.produto.dto.request.ProdutoRequestDTO;
+import br.com.ifba.feiraplus.features.produto.entity.Produto;
+
+import java.util.List;
+
+public interface ProdutoIService {
+    List<Produto> findAll();
+    Produto save(ProdutoRequestDTO produtoDto);
+    Produto update(Long id, ProdutoRequestDTO produtoDto);
+    void delete(Long id);
+    Produto findById(Long id);
+}
